90322aa4290a102a1a4b5efac5025d2d14cb5300
Luis Pastenes <lpastenesm@gmail.com>
2024-11-05 10:49:48 -0300

    Agrega Fix que permite transformar antes que validar en pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // Nota: Configura los pipes globales para la validación y transformación de datos.
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(3000);
 }
 bootstrap();
